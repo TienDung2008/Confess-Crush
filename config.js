a6ec370d4435ff761de21494e516f6fda7bb5b29
TienDung2008 <95042342+TienDung2008@users.noreply.github.com>
2021-11-27 19:03:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "gửi cho Vinh moi",
-    introTitle: 'Vinh gay',
+    titleWeb: "gửi cho Nga Quynh",
+    introTitle: 'Nga Quynh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
